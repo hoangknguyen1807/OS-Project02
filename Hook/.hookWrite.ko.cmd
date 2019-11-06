43ac0cb62b0c6bfde3e8029113606691314f42fd
Nguyễn Vinh <53845497+vinhnguyen604@users.noreply.github.com>
2019-11-06 23:27:57 +0700

    Delete .hookWrite.ko.cmd

--- Hook/.hookWrite.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/OS-Project02/Hook/hookWrite.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /root/OS-Project02/Hook/hookWrite.ko /root/OS-Project02/Hook/hookWrite.o /root/OS-Project02/Hook/hookWrite.mod.o ;  true
