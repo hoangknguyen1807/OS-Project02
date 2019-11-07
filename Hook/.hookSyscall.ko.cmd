60d67e5f06b17db40cd29fa8667fdf1cd8a741e6
Nguyễn Vinh <53845497+vinhnguyen604@users.noreply.github.com>
2019-11-07 12:50:23 +0700

    Delete .hookSyscall.ko.cmd

--- Hook/.hookSyscall.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/OS-Project02/Hook/hookSyscall.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /root/OS-Project02/Hook/hookSyscall.ko /root/OS-Project02/Hook/hookSyscall.o /root/OS-Project02/Hook/hookSyscall.mod.o ;  true
